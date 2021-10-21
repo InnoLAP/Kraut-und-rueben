e110732a487a1943638d3eb28ae06e1e70b0a694
Lauren Hobolt <lauren.hobolt@gmail.com>
2021-10-21 11:10:34 +0200

    Main Page & Profile Settings
    - Main page, where the user can select whether he/she wants to buy ingredients or recipes
    - Profile Settings page is not yet ready
    - CSS changes with the styles to match the changes

---
+++ fe/mainpage.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <title>Kraut und Rüben</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+<div class="header">
+    <button class="button-profile">Profile Settings</button>
+    <div class="header-logo">KRAUT &<br> RÜBEN</div>
+    <button class="button-profile">Shopping Cart</button>
+</div>
+<div class="choose-recipes-ingredients-text">
+    <br> Hier bei Kraut und Rüben können Sie entweder einzelne <br> Zutaten kaufen oder, wenn Sie Inspiration brauchen, ganze Rezepte!
+    <br><br>Was wünschen Sie sich heute?<br>
+</div>
+<div class="mainpage-div">
+    <div class="recipes-image">
+        <button class="recipe-button"></button>
+    </div>
+    <div class="ingredients-image">
+        <button class="ingredient-button"></button>
+    </div>
+</div>
+<?php
+?>
+</body>
+</html>
---
+++ fe/profilesettings.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <title>Kraut und Rüben</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+<div class="header">
+    <button class="button-profile">Profile Settings</button>
+    <div class="header-logo">KRAUT &<br> RÜBEN</div>
+    <button class="button-profile">Shopping Cart</button>
+</div>
+
+<div class="profile-settings-main-div">
+
+</div>
+
+<?php
+?>
+</body>
+</html>
