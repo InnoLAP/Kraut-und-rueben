2efa6fd95f8811b0a01f5c9e35627f29a76e5749
sacura2 <91477978+sacura2@users.noreply.github.com>
2021-10-24 19:43:43 +0200

    register update

--- fe/register.php
+++ fe/register.php
@@ -6,6 +6,28 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
+        <style>
+        label{
+          position: relative;
+          width:300px;
+          margin:20px;
+          padding:5px;
+          font-size:20px;
+          }
+          input[type=text], input[type=datetime], input[type=tel], input[type=password] {
+          width: 70%;
+          padding: 4px 30px;
+          margin: 2px 0;
+          box-sizing: content-box;
+          border: none;
+          border-bottom: 2px solid red;
+          }
+          .required:after {
+            content:" *";
+            color: white;
+            text-shadow:0px 0px;
+          }
+        </style>
     </head>
     <body>
 
@@ -14,56 +36,52 @@
         </div>
         <div class="index-image">
             <div class="register-box2">
-                <br>Registrieren
-                <!-- <form id="loginForm" method="post"> -->
-                  <div class="register">
-                    <label for="VORNAME">Vorname:</label>
+                <h2>Registrierungsformular:</h2>
+                <form>
+
+                    <label class="required" for="VORNAME">Vorname:</label>
                     <input type="text" name="VORNAME" placeholder="Vorname" required><br>
-                  </div>
-                  <div class="register">
-                    <label for="NACHNAME">Nachname:</label>
+
+                    <label class="required" for="NACHNAME">Nachname:</label>
                     <input type="text" name="VORNAME" placeholder="Nachname" required><br>
-                  </div>
-                  <div class="register">
-                    <label for="GEBURTSDATUM">Geburtsdatum:</label>
+
+                    <label class="required" for="GEBURTSDATUM">Geburtsdatum:</label>
                     <input type="datetime" name="GEBURTSDATUM" placeholder="dd.mm.yyyy" required><br>
-                  </div>
-                  <div class="register">
-                    <label for="STRASSE">Straße:</label>
+
+                    <label class="required" for="STRASSE">Straße:</label>
                     <input type="text" name="STRASSE" placeholder="Blumenstraße" required><br>
-                  </div>
-                  <div class="register">
-                    <label for="HAUSNR">Hausnummer:</label>
+
+                    <label class="required" for="HAUSNR">Hausnummer:</label>
                     <input type="text" name="HAUSNR" placeholder="123" required><br>
-                  </div>
-                  <div class="register">
-                    <label for="PLZ">Postleitszahl:</label>
+
+                    <label class="required" for="PLZ">Postleitszahl:</label>
                     <input type="text" name="PLZ" placeholder="80331" required><br>
-                  </div>
-                  <div class="register">
-                    <label for="ORT">Wohnort:</label>
+
+                    <label class="required" for="ORT">Wohnort:</label>
                     <input type="text" name="ORT" placeholder="München" required><br>
-                  </div>
-                  <div class="register">
-                    <label for="TELEFON">Telefonnummer:</label>
+
+                    <label class="required"for="TELEFON">Telefonnummer:</label>
                     <input type="tel" name="TELEFON" placeholder="089-1337" required><br>
-                  </div>
-                  <div class="register">
-                    <label for="email">E-mail: </label>
+
+                    <label class="required" for="email">E-mail: </label>
                     <input type="text" name="email" placeholder="Email" required><br>
-                  </div>
-                  <div class="register">
-                    <label for="password">Passwort: </label>
+
+                    <label class="required" for="password">Passwort: </label>
                     <input type="password" name="password" placeholder="Passwort" required><br>
-                  </div>
+
                 </form>
+<br><br>
             <div class="loginbutton">
                 <div class="confirm-button">
                     <input type="submit" form="loginForm" name="loginBtn" value="Regristrieren" class="bacon">
                 </div>
                 <div class="cancel-button"><a href="index.php">Schließen</a></div>
             </div>
+            </div>
+
+
             <?php
+            /*
                 include "functions.php";
                 include "dbConnect.php";
 
@@ -88,8 +106,9 @@
                                 <p>Dieser Login ist uns unbekannt<br>Email oder Passwort ist falsch</p>
                             </div>');
                     }
-                }
+                } */
             ?>
+
       </div>
     </body>
 </html>
