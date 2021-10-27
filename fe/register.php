0839e4a460ea460cd8c067b953035f84ee7c1a44
sacura2 <91477978+sacura2@users.noreply.github.com>
2021-10-27 18:45:25 +0200

    changed order
    PLZ before town

--- fe/register.php
+++ fe/register.php
@@ -24,7 +24,7 @@
         $telephone=$_POST["telephone"];
         $password=$_POST["password"];
         $birthday=$_POST["birthday"];
-        
+
         if(CheckEmail(null, $email, $db)) {
             AddKunde($name, $surname, $birthday, $password, $street, $houseNr, $zip, $city, $telephone, $email, $db);
 
@@ -60,32 +60,32 @@
 
                     <label class="required" for="NACHNAME">Nachname:</label>
                     <input type="text" name="surname" <?php echo('value="'.$surname.'"')?> placeholder="Nachname" required><br>
-                    
+
                     <label class="required" for="email">E-mail: </label>
                     <input type="email" name="email" <?php echo('value="'.$email.'"')?> placeholder="Email" required><br>
-                    
+
                     <label class="required" for="STRASSE">Straße:</label>
                     <input type="text" name="street" <?php echo('value="'.$street.'"')?> placeholder="Blumenstraße" required><br>
-                    
+
                     <label class="required" for="HAUSNR">Hausnummer:</label>
                     <input type="number" name="houseNr" <?php echo('value="'.$houseNr.'"')?> placeholder="123" required><br>
-                    
-                    <label class="required" for="ORT">Wohnort:</label>
-                    <input type="text" name="city" <?php echo('value="'.$city.'"')?> placeholder="München" required><br>
-                    
+
                     <label class="required" for="PLZ">Postleitszahl:</label>
                     <input type="number" name="zip" <?php echo('value="'.$zip.'"')?> placeholder="80331" required><br>
-                    
+
+                    <label class="required" for="ORT">Wohnort:</label>
+                    <input type="text" name="city" <?php echo('value="'.$city.'"')?> placeholder="München" required><br>
+                                        
                     <label class="required"for="TELEFON">Telefonnummer:</label>
                     <input type="tel" name="telephone" <?php echo('value="'.$telephone.'"')?> placeholder="089-1337" required><br>
-                    
+
                     <label class="required" for="password">Passwort: </label>
                     <input type="password" name="password" placeholder="Passwort" required><br>
-                    
+
                     <label class="required" for="GEBURTSDATUM">Geburtsdatum:</label>
                     <input type="date" name="birthday" <?php echo('value="'.$birthday.'"')?> placeholder="dd.mm.yyyy" required><br>
 
-                    
+
                 </form>
                 <br><br>
                 <input type="checkbox" form="regForm" required>Ich habe die <a class="agbLink" href="agb.html">AGB</a> gelesen und erkläre mich einverstanden *</input>
